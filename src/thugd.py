dd3240089bf16cf845edd2abc58c65ca744d9434
Kichang Yang <manicstreetcoders@gmail.com>
2014-03-03 06:52:35 -0800

    copytree() bug fix

--- src/thugd.py
+++ src/thugd.py
@@ -112,8 +112,20 @@ class Thugd():
             return None
 
         respath = os.path.join(self.resdir, str(job["id"]))
-        shutil.copytree(frompath, respath)
+        self.copytree(frompath, respath)
         return os.path.relpath(respath, self.resdir)
+
+    def copytree(self, src, dst, symlinks=False, ignore=None):
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        for item in os.listdir(src):
+            s = os.path.join(src,item)
+            d = os.path.join(dst,item)
+            if os.path.isdir(s):
+                self.copytree(s, d, symlinks, ignore)
+            else:
+                if not os.path.exists(d) or os.stat(src).st_mtime - os.stat(dst).st_mtime > 1:
+                    shutil.copy2(s,d)
 
     def process(self, job):
         """
