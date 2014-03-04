fdb0302ce10d285ffb9bf0c47131a7c7df532679
Kichang Yang <manicstreetcoders@gmail.com>
2014-03-04 00:09:32 -0800

    install pip with apt-get

--- utils/setup-ubuntu.sh
+++ utils/setup-ubuntu.sh
@@ -4,9 +4,7 @@
 #based on https://github.com/buffer/thug/blob/master/doc/source/build.rst
 
 echo "Installing needed libraries and tools..."
-sudo apt-get install subversion git python build-essential python-setuptools libboost-python-dev libboost-thread-dev python-dev git-core autoconf libtool
-echo "Installing pip..."
-sudo easy_install pip
+sudo apt-get install subversion git python build-essential python-setuptools libboost-python-dev libboost-thread-dev python-dev git-core autoconf libtool python-pip
 
 echo 'Please wait, checking out subversion repo for [http://v8.googlecode.com/svn/trunk/]...'
 svn checkout -r14110 http://v8.googlecode.com/svn/trunk/ v8 1>setup-ubuntu.log
